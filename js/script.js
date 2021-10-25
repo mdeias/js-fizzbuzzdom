d3bf869aea40fcf555ab06c5897b61295ef4b8fc
mdeias <mdeias@icloud.com>
2021-10-25 16:04:01 +0200

    condizioni

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@ const contenitore = document.querySelector(".row");
 for (let i = 1; i <= 100; i++) {
     const box = document.createElement("div");
     box.className = "box";
-    console.log(i);
-    console.log(box);
     box.innerHTML = i;
     contenitore.append(box);
+
+// condizioni
+
+    if (!(i % 3)) {
+        box.classList.add("per-3");
+    }
+
+    if (!(i % 5)) {
+        box.classList.add("per-5");
+    }
+
+    if (!(i % 5) && !(i % 3)) {
+        box.classList.add("comuni");
+    }
 }
+
